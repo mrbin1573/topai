cb1d155342270e070e67a45e468f019e5052dd3b
hebin <20415641@ml.com>
2024-04-10 16:29:03 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/topai/dist/',
+    base: './',
     resolve: {
         alias: {
             //这里猜测应该是一个map，key为别名，value为你要别名的路径
