0f09c991ce29912978c5c5288f248512df3c199e
hebin <20415641@ml.com>
2024-04-10 16:25:41 +0800

    feat 测试部署

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: '/topai/',
     resolve: {
         alias: {
             //这里猜测应该是一个map，key为别名，value为你要别名的路径
